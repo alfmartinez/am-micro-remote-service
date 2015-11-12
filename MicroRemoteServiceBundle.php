674517bb9feec873d42e34566560368177a16acf
Alexandre Martinez <alexandre.martinez76@gmail.com>
2015-11-12 11:40:49 +0100

    Maj composer.json

---
+++ MicroRemoteServiceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Micro\RemoteServiceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MicroRemoteServiceBundle extends Bundle
+{
+}
---
+++ Services/RemoteServiceFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Micro\RemoteService\Services;
+
+use AppBundle\Services\Rest\Client;
+use Httpful\Request;
+
+/**
+ * Description of RemoteServiceFactory
+ *
+ * @author alex
+ */
+class RemoteServiceFactory {
+
+    public static function createService($name, $className, $registryUrl) {
+        $url = self::getServiceUrl($name, $registryUrl);
+        $client = new Client($url);
+        $service = new $className($client);
+        return $service;
+    }
+
+    private static function getServiceUrl($name, $registryUrl) {
+        $uri = $registryUrl . "/api/services/$name/provider";
+        $response = Request::get($uri)
+                ->expectsJson()
+                ->withXTrivialHeader('Asking registry')
+                ->send();
+        return $response->body->url;
+    }
+
+}
