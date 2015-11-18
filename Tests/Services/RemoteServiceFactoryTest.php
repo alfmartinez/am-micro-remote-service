c4ee48fba1cf00b0f8f740f1dfc4c49b0334264b
Alexandre Martinez <alexandre.martinez76@gmail.com>
2015-11-18 16:43:42 +0100

    Base CI

---
+++ Tests/Services/RemoteServiceFactoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Micro\RemoteServiceBundle\Services;
+
+class RemoteServiceFactoryTest extends \PHPUnit_Framework_TestCase {
+
+    public function testHere() {
+        $this->markTestIncomplete();
+    }
+
+}
